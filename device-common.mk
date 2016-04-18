d43167fde8b8a0b4144b5867654c8ff161451ff4
CTXz <ctx.xda@gmail.com>
2016-04-18 21:52:40 +0200

    gts2-common: Move device specific config lists to variant trees

--- device-common.mk
+++ device-common.mk
@@ -149,8 +149,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml  \
-    $(LOCAL_PATH)/configs/media/media_codecs.xml:system/etc/media_codecs.xml \
-    $(LOCAL_PATH)/configs/media/media_profiles.xml:system/etc/media_profiles.xml
 
 # Audio codecs
 PRODUCT_PACKAGES += \
@@ -199,14 +197,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     lights.universal5433
-
-###########################################################
-### GPS
-###########################################################
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/gps/gps.conf:system/etc/gps.conf \
-    $(LOCAL_PATH)/configs/gps/gps.xml:system/etc/gps.xml
 
 ###########################################################
 ### CAMERA
